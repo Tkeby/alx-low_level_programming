161c9e00c174b5d50169b05a33ab877a9cde1905
Tkeby <49399770+Tkeby@users.noreply.github.com>
2022-04-11 15:57:51 +0000

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * the function prints Programming is like building a multilingual puzzle, 
+ * followed by a new line.
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
