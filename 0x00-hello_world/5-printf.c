2a740378afc63ede043b0527961ec0c6d5f902ff
Tkeby <49399770+Tkeby@users.noreply.github.com>
2022-04-11 15:58:54 +0000

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * this function prints quote ""with proper grammar, but the outcome is a piece of art" followed by a new line.
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
