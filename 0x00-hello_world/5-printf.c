45ade3c63d79eebe35533a7fcfde4359e355a870
Tkeby <49399770+Tkeby@users.noreply.github.com>
2022-04-11 16:16:58 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * this function prints quote ""with proper grammar, but the outcome is a piece of art" followed by a new line.
+ * main - Prints quote
  *
  * Return: zero on success
  *
