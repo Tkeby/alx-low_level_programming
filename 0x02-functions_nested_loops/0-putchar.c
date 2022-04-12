053a2827731ada74e2554c4a9efaaa48db5374f5
Tkeby <49399770+Tkeby@users.noreply.github.com>
2022-04-12 13:23:11 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,28 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
 #include "main.h"
 
 /**
- * main - Prints _putchar
- * @void: Empty parameter list for main.
+ * main - Prints "_putchar" followed by a new line.
  *
- * Description: Prints _putchar
- *
- * Return: 0 for success
+ * Success: Return 0.
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
 }
