c884ad6250522fc68afb053f2674a70863782799
Tkeby <49399770+Tkeby@users.noreply.github.com>
2022-04-12 18:04:59 +0000

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,6 +9,18 @@
  * Description: print_alphabet_x10 - prints aphabet 10 times
  *
  **/
+ void print_alphabet(void)
+ {
+
+ 	char i;
+
+ 	for (i = 'a'; i <= 'z'; i++)
+ 	{
+ 		_putchar(i);
+ 	}
+ 	_putchar('\n');
+ }
+
 void print_alphabet_x10(void)
 {
 
