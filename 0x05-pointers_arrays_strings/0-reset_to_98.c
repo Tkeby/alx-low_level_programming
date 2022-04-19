63b712fef9c7a4db20f72ad947b0bed9d3a34705
Tkeby <49399770+Tkeby@users.noreply.github.com>
2022-04-19 15:54:44 +0000

    updated 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 
 /**
- * reset_to_9.c  a function that takes a pointer to an int as parameter and
+ * reset_to_9.c  a function that takes a pointer to
+ * an int as parameter and
  * updates the value it points to to 98.
  * @n: is a pointer int type
  * Return: Always 0.
