5f84429bd50a706224b6698cf95fb208d89eec5c
Tkeby <49399770+Tkeby@users.noreply.github.com>
2022-04-19 15:38:30 +0000

    updated 6-ptus2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * puts2 - a function that prints every other character of a string, starting with the first character, followed by a new line.
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
  * @str: pointer int type
  * Return: Always 0.
  */
